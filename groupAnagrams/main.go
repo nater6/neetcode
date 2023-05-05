7b69754c834f4b728573a5f3c4873bf377a6acee
Nathaniel Russell <n.d.russell@hotmail.com>
2023-05-05 12:56:53 +0100

    Complete with comments and cleaner code

--- groupAnagrams/main.go
+++ groupAnagrams/main.go
@@ -4,12 +4,25 @@ import "strings"
 
 func groupAnagrams(strs []string) [][]string {
 	result := make([][]string, 0)
+	//Loop through the slice that we are checking
 	for _, word := range strs {
+
 		var hasBeenAdded bool
-		for i, anagramContainer := range result {
-			anagramLoop: for _, anagram := range anagramContainer {
+		//Go through all the anagrams found already
+		for i, differentAnagrams := range result {
+			//Get the first word in each anagram group
+			anagramLoop: for _, anagram := range differentAnagrams {
+				//Check if the words are a different length, or if one is empty and the other is not
+				isSameLength := len(word) == len(anagram)
+				isEmptyString :=  anagram == "" 
+				isSameWord := anagram == word
+				if !isSameLength || isEmptyString && !isSameWord {
+					break
+				}
+				//check each character of the anagram and make sure it is present the same amount of times in both words
 				for _, c := range anagram {
-					if !strings.Contains(word, string(c)){
+					hasSameCount := strings.Count(word, string(c)) == strings.Count(anagram, string(c))
+					if !hasSameCount {
 						break anagramLoop
 					}
 				}
@@ -19,6 +32,7 @@ func groupAnagrams(strs []string) [][]string {
 				break
 			}
 		}
+		//If the word wasn't added to any anagram group create a new anagram group containing the word
 		if !hasBeenAdded {
 			result = append(result, []string{word})
 		}
