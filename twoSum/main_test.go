c6f629d135d7a1710b0f2e667cb1aeb46efc9142
Nathaniel Russell <n.d.russell@hotmail.com>
2023-05-05 11:48:40 +0100

    Test structure set up

---
+++ twoSum/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+
+func TestTwoSum(t *testing.T) {
+	testCases := []struct{
+		result []int
+		expected []int
+	}{
+		{twoSum([]int{2,7,11,15}, 9),[]int{0,1}},
+		{twoSum([]int{3,2,4},6),[]int{1,2}},
+		{twoSum([]int{3,3}, 6),[]int{0,1}},
+	}
+
+	for _, test := range testCases {
+		if reflect.DeepEqual(test.expected, test.result) {
+			t.Errorf("Expected != Result: ", test)
+		}
+	} 
+	
+}
