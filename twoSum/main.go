d7bf78710a68ddcb9d1d128c6dc8ecc1dd4e49d9
Nathaniel Russell <n.d.russell@hotmail.com>
2023-05-05 11:54:13 +0100

    Completed

--- twoSum/main.go
+++ twoSum/main.go
@@ -1,5 +1,16 @@
 package main
 
 func twoSum(nums []int, target int) []int {
+	//Iterate through each index of nums
+	for i, n := range nums {
+		//Iterate again skip the current index
+		for j, x := range nums {
+			//check if result = target
+			if j != i && n+x == target {
+				return []int{i, j}
+			}
+		}
+
+	}
 	return []int{}
 }
